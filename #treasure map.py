71d78dbb1a3b23deef260fbe8087ef433b618f92
dreamsongg <smasherkao@gmail.com>
2025-02-16 17:08:03 -0700

    add new exercise treasure map

--- #treasure map.py
+++ #treasure map.py
@@ -11,6 +11,18 @@ print(f"{row1}\n{row3}\n{row3}\n")
 position = input("Where would you like to hide the treasure?\n")
 
 
-position = treasure_placement.choice(map)
 
-print(treasure_placement)
+row_index = int(position[0]) - 1 
+col_index = int(position[1]) - 1
+
+if row_index > 2:
+    row_index = 2
+    print("This row is out of range")
+if col_index > 2:
+    col_index = 2
+    print("This column is out of range")
+
+map[row_index][col_index] = "x"
+
+
+print(map)
